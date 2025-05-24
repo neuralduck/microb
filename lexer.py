79667a474fc63d8375c18864d8391d1a62f5f019
hari <theneuralduck@gmail.com>
2025-05-25 00:16:04 +0530

    updated lexer. not completed yet

--- lexer.py
+++ lexer.py
@@ -1,58 +1,83 @@
-from enum import Enum, auto
+from enum import StrEnum, auto
 from dataclasses import dataclass
 
 
-class TokenType(Enum):
-    IDENTIFIER = auto()
-    NUMBER = auto()
-    STRING = auto()
-    BOOLEAN = auto()
-
+class TokenType(StrEnum):
     KEYWORD = auto()
-    KW_PRINT = auto()
+    IDENTIFIER = auto()
+    NUMBER_LITERAL = auto()
+    STRING_LITERAL = auto()
 
-    ARITHMETIC_OP = auto()
-    LOGICAL_OP = auto()
-    COMPARISON_OP = auto()
+    ADD = auto()
+    SUB = auto()
+    MUL = auto()
+    DIV = auto()
+    MOD = auto()
+    
+    AND = auto()
+    OR = auto()
 
-    SEPARATOR = auto()
+    NOT = auto()
+    
+    EQUAL = auto()
+    ASSIGN = auto()
+    NOT_EQUAL = auto()
+    LESS_THAN = auto()
+    GREATER_THAN = auto()
+    LESS_OR_EQUAL = auto()
+    GREATER_OR_EQUAL = auto()
 
-    EOF = auto()
+    OPEN_PAREN = auto()
+    CLOSE_PAREN = auto()
+    OPEN_BRACE = auto()
+    CLOSE_BRACE = auto()
+    COMMA = auto()
+    SEMICOLON = auto()
+    COLON = auto()
+
+    NEW_LINE = auto()
+
 @dataclass
 class Token:
-    tokentype: TokenType
-    value: str
+    token_type: TokenType
+    value: str | None
 
-class Buffer:
-    def __init__(self):
-        self.buffer = ''
-    def add(self, c):
-        self.buffer += c
-    def flush(self):
-        self.buffer = ''
-    def __repr__(self):
-        return self.buffer
-KEYWORDS = ["print", "num", "str", "if", "else", "while", "continue", "break", "fn", "return", "null", "true", "false"]
-SEPARATORS = ["#", ",", ";", "(", ")", ""]
-OPERATORS = []
-
-
-
-filepath = "./sample.mb"
-with open(filepath, 'r') as f: code = f.read()
-
+with open("sample.mb", "r") as f:
+    code = f.read()
 
 tokens = []
 i = 0
-codelen = len(code)
-buffer = Buffer()
-while i < codelen:
+buffer = ''
+
+while i < len(code):
+    if code[i] == ' ':
+        i += 1
+        continue
+
     if code[i] == '#':
-        tokens.append(Token(TokenType.SEPARATOR, code[i]))
-    if code[i] == "\n":
-        tokens.append(Token(TokenType.SEPARATOR, code[i]))
-        
+        while code[i] != '\n':
+            i+=1
+        continue
+    
+    if code[i] == '"':
+        i += 1
+        j = i
+        while code[i] != '"':
+            i += 1
+        #print("string literal")
+        #print(code[j:i])
+        tokens.append(Token(TokenType.STRING_LITERAL, code[j:i]))
+    
+    if code[i].isnumeric():
+        j = i
+        i += 1
+        while(code[i].isnumeric() or code[i] == '.'):
+            i+=1
+        #print("number literal")
+        #print(code[j:i])
+        tokens.append(Token(TokenType.NUMBER_LITERAL, code[j:i]))
+    
     i+=1
 
-print(tokens)
-    
+print(*tokens)
+        
